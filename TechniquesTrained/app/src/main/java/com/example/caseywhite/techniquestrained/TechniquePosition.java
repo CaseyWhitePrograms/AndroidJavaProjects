3f8e509eb242ae9bb6e4e558e028b1f0bd34169f
CaseyWhitePrograms <cgw21686@gmail.com>
2019-10-16 22:49:23 -0400

    AndroidJavaProject
    AndroidJavaProject

---
+++ TechniquesTrained/app/src/main/java/com/example/caseywhite/techniquestrained/TechniquePosition.java
@@ -0,0 +1,74 @@
+
+package com.example.caseywhite.techniquestrained;
+
+public class TechniquePosition
+{
+    // fields
+    String position;
+    String attack;
+    String rank;
+    String hours;
+    String date;
+
+    // constructor. Parameters for all fields
+    public TechniquePosition(String position, String attack, String rank,
+                             String hours, String date)
+    {
+        // call the superclass constructor
+        this.position = position;
+        this.attack = attack;
+        this.rank = rank;
+        this.hours = hours;
+        this.date = date;
+    }
+
+
+    public String toString()
+    {
+        return position + ", " + attack + ", " + rank + ", " + hours + ", " + date  ;
+    }
+
+
+    //public get/set methods
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getAttack() {
+        return attack;
+    }
+
+    public void setAttack(String attack) {
+        this.attack = attack;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    public void setHours(String hours) {
+        this.hours = hours;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+}
